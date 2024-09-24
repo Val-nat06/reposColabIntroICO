4e8629df33e4494de8898f8c83e1769292aac562
Val <valenatglez06@hotmail.com>
2024-09-23 22:09:27 -0600

    Create programahexagono.cpp
    PROGRAMA PERÍMETRO HEXÁGONO

---
+++ programahexagono.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float lado, perimetro;
+    printf("Ingrese la medida de lado del hexágono: ");
+    scanf_s("%f", &lado);
+
+    perimetro = 6 * lado;
+    printf("El perímetro del hexágono es: %.2f", perimetro);
+
+    return 0;
+}
